d8e6f3aec9c96c634e3cf57db1ba84ee54fefbfe
hugo4295 <hugo_4295@hotmail.com>
2020-04-10 17:53:21 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/hugo4295/ejercicio20201/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.hugo4295.ejercicio20201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
